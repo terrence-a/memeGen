935c49561ac544410aea0c83982da25c69d14dbe
Terrence Amponsah <tamponsah9909@gmail.com>
2019-11-13 00:39:00 -0500

    Setup Header.js and Memegenerator.js, for the structure of the app

--- memegen/src/App.js
+++ memegen/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MemeGenerator from './components/MemeGenerator';
 
 function App() {
   return (
-    <Header />
+    <div>
+      <Header />
+      <MemeGenerator />
+    </div>
   );
 }
 
---
+++ memegen/src/components/MemeGenerator.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class MemeGenerator extends Component {
+    render(){
+        return(
+            <p>This is where the memeGenerating will take place (dun, dun duuuun)</p> 
+        )
+    }
+}
+
+export default MemeGenerator
